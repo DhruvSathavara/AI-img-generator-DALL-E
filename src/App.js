765228b0c465cf56a1b05cfa16799753a848c3dd
DhruvSathavara <dhruvsathavara7228@gmail.com>
2023-05-01 09:57:05 +0530

    ai-img-generator

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ImageGenerator from './components/ImageGenerator';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ImageGenerator />
+      </header>
+    </div>
+  );
+}
+
+export default App;
